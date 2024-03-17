989b42c0a56f4c778cef2d9531cc22467f3bb2e1
liyixin <lyxiiin@qq.com>
2024-03-17 21:37:44 +0800

    第一次成功创建ui并转为代码运行

---
+++ run_ui.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from main_window_ui import Ui_MainWindow  # 从你的 UI 文件中导入 UI 类
+
+
+def main():
+    import sys
+    app = QApplication(sys.argv)
+    main_window = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(main_window)
+    main_window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
