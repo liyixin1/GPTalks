74da8a7c466a60b0ff11eeb8594ac23e2cc0d227
liyixin <lyxiiin@qq.com>
2024-03-18 23:38:56 +0800

    抽象出窗体类

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication
 
-from main_window_ui import Ui_MainWindow
+from main_window import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
-    main_window = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(main_window)
-    main_window.show()  # 显示窗体
+    window = MainWindow()
+    window.show()
+
     return_value = app.exec()  # 监听事件直到窗体退出
     sys.exit(return_value)  # 回收资源并向系统返回值
 
---
+++ main_window.py
@@ -0,0 +1,10 @@
+from PyQt6.QtWidgets import QMainWindow
+
+from main_window_ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
