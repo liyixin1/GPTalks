085140521051e6c99091104bfe74fbf47b89b7da
liyixin <lyxiiin@qq.com>
2024-03-20 00:10:35 +0800

    新增通过点击会话名进入该会话并显示在session_name

--- ui/main_window.py
+++ ui/main_window.py
@@ -9,11 +9,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.pushButton_commit.clicked.connect(self.on_commit_button_clicked)
+        self.listWidget_session.currentItemChanged.connect(self.on_current_item_changed)
+
         list_item = ListWidgetItem("会话1")
+        list_item_2 = ListWidgetItem("会话2")
         self.listWidget_session.addItem(list_item)
+        self.listWidget_session.addItem(list_item_2)
         self.listWidget_session.setCurrentItem(list_item)
 
     def on_commit_button_clicked(self):
         self.textBrowser_show.setText(
             self.listWidget_session.currentItem().get_record(self.plainTextEdit_input.toPlainText()))
         self.plainTextEdit_input.clear()
+
+    def on_current_item_changed(self):
+        self.lineEdit_name.setText(self.listWidget_session.currentItem().text())
