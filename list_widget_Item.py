709eab3b8feaa7aa4604ac043fe03693d577cc76
liyixin <lyxiiin@qq.com>
2024-03-20 11:56:18 +0800

    新增通过ui交互新建会话和删除会话功能，并且当会话列表为空时与chatgpt对话会新建一个会话并设置为当前会话

--- list_widget_Item.py
+++ list_widget_Item.py
@@ -10,15 +10,18 @@ class ListWidgetItem(QtWidgets.QListWidgetItem):
         self.record = []
 
     def get_record(self, user_input) -> str:
+        if user_input:
+            self.record.append(user_input_to_record_item(user_input))
+            chatgpt_reply = get_gpt_response(self.record)
+            self.record.append(chatgpt_reply)
+
+        return self.record_to_display_text()
+
+    def record_to_display_text(self) -> str:
         return_record = []
-        self.record.append(user_input_to_record_item(user_input))
-        chatgpt_reply = get_gpt_response(self.record)
-        self.record.append(chatgpt_reply)
-        print(self.record)
         for i in self.record:
             if i['role'] == "user":
                 return_record.append("你：" + i['content'])
             else:
                 return_record.append("chatGPT:" + i['content'])
-
         return '\n'.join(return_record)
--- ui/main_window.py
+++ ui/main_window.py
@@ -8,19 +8,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton_commit.clicked.connect(self.on_commit_button_clicked)
-        self.listWidget_session.currentItemChanged.connect(self.on_current_item_changed)
-
-        list_item = ListWidgetItem("会话1")
-        list_item_2 = ListWidgetItem("会话2")
-        self.listWidget_session.addItem(list_item)
-        self.listWidget_session.addItem(list_item_2)
-        self.listWidget_session.setCurrentItem(list_item)
+        self.pushButton_commit.clicked.connect(self.on_commit_button_clicked)  # 提交按钮点击信号
+        self.listWidget_session.currentItemChanged.connect(self.on_current_item_changed)  # 鼠标点击会话列表项信号
+        self.pushButton_new.clicked.connect(self.on_new_button_clicked)  # 新建会话按钮点击信号
+        self.pushButton_delect.clicked.connect(self.on_delect_button_clicked)  # 删除会话按钮点击信号
 
     def on_commit_button_clicked(self):
+        if self.listWidget_session.count() == 0:  # 如果当前不存在会话记录，则新建一个
+            self.on_new_button_clicked()
         self.textBrowser_show.setText(
             self.listWidget_session.currentItem().get_record(self.plainTextEdit_input.toPlainText()))
         self.plainTextEdit_input.clear()
 
     def on_current_item_changed(self):
         self.lineEdit_name.setText(self.listWidget_session.currentItem().text())
+        self.textBrowser_show.setText(self.listWidget_session.currentItem().record_to_display_text())
+
+    def on_new_button_clicked(self):
+        new_item = ListWidgetItem("会话" + str(self.listWidget_session.count() + 1))
+        self.listWidget_session.addItem(new_item)
+        self.listWidget_session.setCurrentItem(new_item)
+
+    def on_delect_button_clicked(self):
+        del_item = self.listWidget_session.takeItem(self.listWidget_session.currentRow())
+        del del_item
