d239687a6f1884cd66cb0523d312a1d938e04ddf
liyixin1 <lyxiiin@qq.com>
2024-06-19 17:40:15 +0800

    添加并载入样式

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from ui.main_window import MainWindow
 def main():
     """创建主窗口实例"""
     app = QApplication(sys.argv)
+    # 加载QSS样式表
+    with open('qss/main.qss', 'r', encoding='utf-8') as f:
+        qss = f.read()
+    app.setStyleSheet(qss)
     window = MainWindow()
     window.show()
     return_value = app.exec()  # 监听事件直到窗体退出
